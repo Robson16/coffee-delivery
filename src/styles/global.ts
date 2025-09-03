e8f7f59df912974c340bdf1fb76c5382ed404382
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-09-03 16:53:22 -0300

    fix: missed the new right way to use theme variables

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,12 +9,12 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${(props) => props.theme['purple']}
+    box-shadow: 0 0 0 2px ${(props) => props.theme.colors['purple']}
   }
 
   body {
-    background: ${(props) => props.theme['background']};
-    color: ${(props) => props.theme['base-text']};
+    background: ${(props) => props.theme.colors['background']};
+    color: ${(props) => props.theme.colors['base-text']};
     -webkit-font-smoothing: antialiased;
   }
 
