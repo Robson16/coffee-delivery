4dfc7c6b975c1528c65e1eca57b00a0188c2b3db
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-09-03 16:49:18 -0300

    feat: theme typography sizes

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,21 +1,84 @@
 export const defaultTheme = {
-  white: '#FFF',
-  background: '#FAFAFA',
+  colors: {
+    white: '#FFF',
+    background: '#FAFAFA',
 
-  'base-card': '#F3F2F2',
-  'base-input': '#EDEDED',
-  'base-button': '#E6E5E5',
-  'base-hover': '#D7D5D5',
-  'base-label': '#8D8686',
-  'base-text': '#574F4D',
-  'base-subtitle': '#403937',
-  'base-title': '#272221',
+    'base-card': '#F3F2F2',
+    'base-input': '#EDEDED',
+    'base-button': '#E6E5E5',
+    'base-hover': '#D7D5D5',
+    'base-label': '#8D8686',
+    'base-text': '#574F4D',
+    'base-subtitle': '#403937',
+    'base-title': '#272221',
 
-  'purple-light': '#EBE5F9',
-  'purple': '#8047F8',
-  'purple-dark': '#4B2995',
+    'purple-light': '#EBE5F9',
+    'purple': '#8047F8',
+    'purple-dark': '#4B2995',
 
-  'yellow-light': '#F1E9C9',
-  'yellow': '#DBAC2C',
-  'yellow-dark': '#C47F17',
+    'yellow-light': '#F1E9C9',
+    'yellow': '#DBAC2C',
+    'yellow-dark': '#C47F17',
+  },
+
+  typography: {
+    fontFamily: {
+      baloo2: 'Baloo 2',
+      roboto: 'Roboto',
+    },
+    title: {
+      'title-xl': {
+        size: '48px',
+        lineHeight: '130%',
+      },
+      'title-l': {
+        size: '32px',
+        lineHeight: '130%',
+      },
+      'title-m': {
+        size: '24px',
+        lineHeight: '130%',
+      },
+      'title-s': {
+        size: '20px',
+        lineHeight: '130%',
+      },
+      'title-xs': {
+        size: '18px',
+        lineHeight: '130%',
+      },
+    },
+    text: {
+      'text-l': {
+        size: '20px',
+        lineHeight: '130%',
+      },
+      'text-m': {
+        size: '16px',
+        lineHeight: '130%',
+      },
+      'text-s': {
+        size: '14px',
+        lineHeight: '130%',
+      },
+      'text-xs': {
+        size: '12px',
+        lineHeight: '130%',
+      },
+    },
+    tag: {
+      size: '10px',
+      lineHeight: '130%',
+    },
+    button: {
+      'button-g': {
+        size: '14px',
+        lineHeight: '160%',
+      },
+      'button-m': {
+        size: '12px',
+        lineHeight: '160%',
+      },
+    },
+  },
 }
