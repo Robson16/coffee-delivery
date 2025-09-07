9dea83e5a4834d42384d58fecec4758603e62a22
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-09-07 14:30:25 -0300

    feat: home intro

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,62 @@
-import { HomeContainer, Title } from "./styles";
+import { HiShoppingCart } from 'react-icons/hi'
+import { PiCoffeeFill, PiPackageFill, PiTimerFill } from 'react-icons/pi'
+import CoffeeImage from '../../assets/coffee-image.png'
+import {
+  IconContainer,
+  IconList,
+  IconListItem,
+  IntroContainer,
+  IntroWrapper,
+  Subtitle,
+  Title,
+} from './styles'
 
 export function Home() {
   return (
-    <HomeContainer>
-      <Title>Home</Title>
-    </HomeContainer>
+    <IntroWrapper>
+      <IntroContainer>
+        <div>
+          <Title>Encontre o café perfeito para qualquer hora do dia</Title>
+
+          <Subtitle>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </Subtitle>
+
+          <IconList>
+            <IconListItem>
+              <IconContainer bgColor="yellow-dark">
+                <HiShoppingCart size={20} />
+              </IconContainer>
+              Compra simples e segura
+            </IconListItem>
+
+            <IconListItem>
+              <IconContainer bgColor="base-text">
+                <PiPackageFill size={20} />
+              </IconContainer>
+              Embalagem mantém o café intacto
+            </IconListItem>
+
+            <IconListItem>
+              <IconContainer bgColor="yellow">
+                <PiTimerFill size={20} />
+              </IconContainer>
+              Entrega rápida e rastreada
+            </IconListItem>
+
+            <IconListItem>
+              <IconContainer bgColor="purple">
+                <PiCoffeeFill size={20} />
+              </IconContainer>
+              O café chega fresquinho até você
+            </IconListItem>
+          </IconList>
+        </div>
+        <div>
+          <img src={CoffeeImage} alt="Copo de café" />
+        </div>
+      </IntroContainer>
+    </IntroWrapper>
   )
 }
--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -1,10 +1,100 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import IntroBackground from '../../assets/intro-background.png'
 
-export const HomeContainer = styled.div`
+interface IconContainerProps {
+  bgColor: 'yellow-dark' | 'yellow' | 'base-text' | 'purple'
+}
+
+export const IntroWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-`;
+  align-items: center;
+  justify-content: center;
+  background-image: url(${IntroBackground});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`
+
+export const IntroContainer = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  gap: 3.5rem; // 56px
+  margin-top: 5.875rem; // 94px
+  margin-bottom: 6.75rem; // 108px
+  width: 100%;
+  max-width: ${(props) => props.theme.container.maxWidth};
+  padding: ${(props) => props.theme.container.padding};
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+
+    @media (min-width: 992px) {
+      &:first-child {
+        flex: 0 0 55%;
+      }
+
+      &:last-child {
+        flex: 1;
+      }
+    }
+
+    img {
+      max-width: 100%;
+      height: auto;
+    }
+  }
+`
 
 export const Title = styled.h1`
-  font-size: 2rem;
-`;
+  margin-bottom: 1rem;
+  font-size: ${(props) => props.theme.typography.title['title-xl'].size};
+  font-family: ${(props) => props.theme.typography.fontFamily.baloo2};
+  font-weight: 800;
+  line-height: ${(props) =>
+    props.theme.typography.title['title-xl'].lineHeight};
+  color: ${(props) => props.theme.colors['base-title']};
+`
+
+export const Subtitle = styled.h2`
+  margin-bottom: 4.125rem; // 66px
+  font-size: ${(props) => props.theme.typography.text['text-l'].size};
+  font-weight: 400;
+  line-height: ${(props) => props.theme.typography.text['text-l'].lineHeight};
+  color: ${(props) => props.theme.colors['base-subtitle']};
+`
+
+export const IconList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  gap: 1rem;
+
+  @media (min-width: 992px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`
+
+export const IconListItem = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  font-size: ${(props) => props.theme.typography.text['text-m'].size};
+  color: ${(props) => props.theme.colors['base-text']};
+`
+
+export const IconContainer = styled.span<IconContainerProps>`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 2rem;
+  height: 2rem;
+  padding: 0.5rem;
+  border-radius: 100%;
+  background-color: ${(props) => props.theme.colors[props.bgColor]};
+
+  svg {
+    color: ${(props) => props.theme.colors['white']};
+  }
+`
