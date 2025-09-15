9cb6dbb85d61857db64578d3409ffb34310d6279
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-09-15 13:33:24 -0300

    feat: cart icon with total of products in the cart

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -1,9 +1,20 @@
+import { useContext } from 'react'
 import { HiShoppingCart } from 'react-icons/hi'
+import { CartContext } from '../../contexts/CartContext'
 import { CartIconContainer } from './styles'
 
 export function CartIcon() {
+  const { products } = useContext(CartContext)
+
   return (
     <CartIconContainer to="/cart" title="Carrinho">
+      {products.length > 0 && (
+        <span>
+          {products.reduce((total, product) => {
+            return total + product.quantity
+          }, 0)}
+        </span>
+      )}
       <HiShoppingCart size={24} />
     </CartIconContainer>
   )
--- src/components/CartIcon/styles.ts
+++ src/components/CartIcon/styles.ts
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
 export const CartIconContainer = styled(NavLink)`
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -11,6 +12,24 @@ export const CartIconContainer = styled(NavLink)`
   background-color: ${(props) => props.theme.colors['primary-light']};
   border-radius: 6px;
   transition: all 0.2s;
+
+  span {
+    position: absolute;
+    top: -0.5rem;
+    right: -0.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 1.25rem;
+    height: 1.25rem;
+    padding: 0.25rem;
+    font-weight: 700;
+    font-size: ${(props) => props.theme.typography.text['text-s'].size};
+    line-height: ${(props) => props.theme.typography.text['text-s'].lineHeight};
+    border-radius: 100%;
+    color: ${(props) => props.theme.colors['white']};
+    background-color: ${(props) => props.theme.colors['primary-dark']};
+  }
 
   svg {
     font-size: 1.125rem; // 18px
